df9c38916d5e3eec6b5cc86b19bdb1a09ba4fcb3
danqwas <52236694+danqwas@users.noreply.github.com>
2022-04-15 00:20:05 -0500

    update GuideElement

--- lib/src/pages/GuideElement.dart
+++ lib/src/pages/GuideElement.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 //Create a GridView of 6 elements
@@ -7,25 +9,90 @@ class GuideElement extends StatelessWidget {
     return GridView.count(
       crossAxisCount: 2,
       children: <Widget>[
+        // insert the elements here we have a widget for each element like buttons, text, images etc. like an interface
+
+        //Button in container
         Container(
-          child: Image.asset('lib/src/images/fondohome2osi.png'),
+          //elements in the center of the container
+          // the container ajusts the size of the elements
+          alignment: Alignment.center,
+          child: Expanded(
+            child: Center(
+              //elements in the center of the center
+              // the center ajusts the size of the aplication
+              child: Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(onPressed: () => {}, child: Text("Button")),
+                    Text('Button'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          padding: const EdgeInsets.all(8.0),
         ),
+//field
         Container(
-          child: Image.asset('lib/src/images/fondohome2osi.png'),
-        ),
+            width: 100,
+            padding: const EdgeInsets.all(8.0),
+            child: Expanded(
+              child: Center(
+                child: Card(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 100,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'SearchBar',
+                          ),
+                        ),
+                      ),
+                      // espacio
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('SearchBar'),
+                    ],
+                  ),
+                ),
+              ),
+            )),
+        // search bar using the search bar widget (ganador)
         Container(
-          child: Image.asset('lib/src/images/fondohome2osi.png'),
-        ),
-        Container(
-          child: Image.asset('lib/src/images/fondohome2osi.png'),
-        ),
-        Container(
-          child: Image.asset('lib/src/images/fondohome2osi.png'),
-        ),
-        Container(
-          child: Image.asset('lib/src/images/fondohome2osi.png'),
+          width: 100,
+          padding: const EdgeInsets.all(10.0),
+          child: Expanded(
+            child: Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'SearchBar',
+                      ),
+                    ),
+                  ),
+                  Text('SearchBar'),
+                ],
+              ),
+            ),
+          ),
         ),
       ],
     );
   }
 }
+
+// Create a Enum from widgets like buttons, searchbars, scrollbar,etc
+
